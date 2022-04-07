08b2977e47528bd4ea500129767c1cadcc560bbb
Sergio Virginio dos Santos <sergiologos6@gmail.com>
2022-04-07 20:42:14 -0300

    Recorrendo ao JavaScript

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -5,14 +5,15 @@ export function logarTempoDeExecucao(){
         descriptor: PropertyDescriptor
         ){
             const metodoOriginal = descriptor.value;
-            descriptor.value = function(){
+            descriptor.value = function(...args: Array<any>){
 
                 const t1 = performance.now();
 
-                const retorno = metodoOriginal();
+                const retorno = metodoOriginal.apply(this, args);
 
                 const t2 = performance.now();
-                console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`)
+                console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`);
+                retorno
             };
 
             return descriptor;
